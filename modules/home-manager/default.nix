bb09ae5fae53378f4697548f100497b1de779c03
Sven Meyer <meyer.sven@gmail.com>
2024-05-14 16:07:07 +0200

    Small refactors

--- modules/home-manager/default.nix
+++ modules/home-manager/default.nix
@@ -33,6 +33,7 @@
       changeHunkIndicators = true;
     };
     extraConfig = {
+      # I prefer to have configuration in gitconfig format
       include = { path = "~/.config/git/config_extra"; };
     };
   };
@@ -41,17 +42,18 @@
   };
   home = {
     file = {
+      # These are named "*_extra" because home-manager already puts the normal file in place
       ".config/git/config_extra".source = ./dotfiles/gitconfig;
+      ".config/zshrc_extra".source = ./dotfiles/zshrc;
       ".ssh/config".source = ./dotfiles/ssh_config;
       ".config/alacritty/alacritty.toml".source = ./dotfiles/alacritty.toml;
-      ".config/zshrc_extra".source = ./dotfiles/zshrc;
       ".config/nvim/init.lua".source = ./dotfiles/nvim/init.lua;
       "./Library/Application Support/Sublime Text/Packages/Declarative/Preferences.sublime-settings" = {
         source = ./dotfiles/Preferences.sublime-settings;
       };
     };
     activation = {
-      myActivationAction = lib.hm.dag.entryAfter ["writeBoundary"] ''
+      copyAppSymLinks = lib.hm.dag.entryAfter ["writeBoundary"] ''
         rm -rf "$HOME/Applications/NixLaunchPadFix"
         mkdir "$HOME/Applications/NixLaunchPadFix"
         cp -P "$HOME/Applications/Home Manager Apps"/*.app "$HOME/Applications/NixLaunchPadFix"

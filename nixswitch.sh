e405fe74009afd89dfd18e59ca77c478fe7c858c
Sven Meyer <meyer.sven@gmail.com>
2024-05-14 16:19:51 +0200

    remove nixswitch.sh

--- nixswitch.sh
+++
@@ -1,8 +0,0 @@
-#!/usr/bin/env sh
-
-pushd ~/src/system-config
-
-nix --extra-experimental-features "nix-command flakes" build .#darwinConfigurations.sven-mbp.system \
-  && ./result/sw/bin/darwin-rebuild switch --flake ~/src/system-config
-
-popd

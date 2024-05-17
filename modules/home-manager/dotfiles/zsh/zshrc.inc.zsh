938c31dfd846c6653c519df286dee691cb7da265
Sven Meyer <meyer.sven@gmail.com>
2024-05-17 14:12:05 +0200

    Fixed bug: misspelled local.zshrc.zsh

--- modules/home-manager/dotfiles/zsh/zshrc.inc.zsh
+++ modules/home-manager/dotfiles/zsh/zshrc.inc.zsh
@@ -5,6 +5,6 @@ if [ -x /opt/homebrew/bin/brew ]; then
   eval "$(/opt/homebrew/bin/brew shellenv)"
 fi
 
-source "$HOME/.config/zsh/zshrc.local.zsh"
+source "$HOME/.config/zsh/local.zshrc.zsh"
 
 # End included portion
